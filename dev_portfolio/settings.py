eb4e83d097fe758c61c6dcf863dac08d608c0f59
Walentin <valoliinykov.work@gmail.com>
2026-02-17 19:34:32 +0200

    docker compose

--- dev_portfolio/settings.py
+++ dev_portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
@@ -78,11 +79,17 @@ WSGI_APPLICATION = "dev_portfolio.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+
+    "default":  dj_database_url.config(
+                        default=os.getenv("DATABASE_URL"),
+                        conn_max_age=600
+                        )
     }
-}
+
 
 
 # Password validation
